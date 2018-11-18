79554f32e9f6e3e3dcccf08f41d66158741a1fba
DESKTOP-85K3NK7\Earth <sutthiphongnuanma@gmail.com>
2018-11-18 09:15:38 +0700

    edit access

--- configs/config.js
+++ configs/config.js
@@ -1,16 +1,7 @@
-module.exports = (req,res,next)=>{
-    res.setHeader('Content-Type','application/json');
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+module.exports = (req, res, next) => {
+  res.setHeader('content-type', 'application/json');
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  res.set('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token');
+  next();
 }
